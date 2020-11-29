b4dd69f4834f814352db158910ba72caf71e7a47
ChiranjeeviShinde <74903894+ChiranjeeviShinde@users.noreply.github.com>
2020-11-29 10:31:21 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -2,28 +2,32 @@
 var PLAY = 1;
 var END = 0;
 var gameState = PLAY;
-var monkey, monkey_animation,monkey_collided;
+var monkey, monkey_animation, monkey_collided;
 var banana, bananaImage, obstacle, obstacleImage;
 var bananaGroup, obstacleGroup;
 var score;
 var ground1, ground2;
 var score = 0;
-var survivaltime = 0;
+var bg, bgImage;
 
 function preload() {
-
+  bgImage = loadImage("jungle.png");
 
   monkey_animation = loadAnimation("sprite_0.png", "sprite_1.png", "sprite_2.png", "sprite_3.png", "sprite_4.png", "sprite_5.png", "sprite_6.png", "sprite_7.png", "sprite_8.png")
 
   bananaImage = loadImage("banana.png");
   obstacleImage = loadImage("obstacle.png");
-  
-  monkey_collided = loadImage("sprite_0.png")
+
+  monkey_collided = loadAnimation("sprite_0.png")
 
 }
 
 function setup() {
   createCanvas(600, 600);
+
+  bg = createSprite(300, 300, 100, 100);
+  bg.addImage("jungle", bgImage);
+  bg.velocityX = -4;
 
   //creating monkey
   monkey = createSprite(80, 540, 20, 20);
@@ -35,13 +39,15 @@ function setup() {
   ground1.velocityX = 4;
   ground1.x = ground1.width / 2
   ground1.shapeColor = "lightgreen";
+  ground1.visible = false;
 
 
   //creating ground2
   ground2 = createSprite(500, 595, 500, 10);
   ground2.velocityX = -4;
   ground2.x = ground2.width / 2
-  ground2.shapeColor = "lightgreen"
+  ground2.shapeColor = "lightgreen";
+  ground2.visible = false;
 
   //creating groups
   bananaGroup = new Group();
@@ -63,6 +69,10 @@ function draw() {
       ground2.x = ground2.width / 2;
     }
 
+    if (bg.x < 0) {
+      bg.x = bg.width / 2;
+    }
+
     //making monkey collide with the ground
     monkey.collide(ground1);
     monkey.collide(ground2)
@@ -75,27 +85,45 @@ function draw() {
     //add gravity                                         
     monkey.velocityY = monkey.velocityY + 0.8
 
+    switch (score) {
+      case 10:
+        monkey.scale = 0.16;
+        break;
+      case 20:
+        monkey.scale = 0.18;
+        break;
+      case 30:
+        monkey.scale = 0.21;
+        break;
+      case 40:
+        monkey.scale = 0.25;
+        break;
+      default:
+        break;
+    }
+
     if (monkey.isTouching(bananaGroup)) {
       bananaGroup.destroyEach();
-      score = score + 2;
+      score = score + 5;
     }
 
     if (monkey.isTouching(obstacleGroup)) {
       gameState = END;
+      monkey.scale = 0.15;
     }
 
     //calling the functions
     food();
     obstacles();
-    
-    survivaltime  = Math.ceil(frameCount/frameRate())
-    text("Survival Time: "+survivaltime,100,50,fill("black"),textSize(15));
-    
+
   } else if (gameState === END) {
     monkey.velocityY = 0;
     ground1.velocityX = 0;
     ground2.velocityX = 0;
-    
+    bg.velocityX = 0;
+
+    monkey.changeAnimation("collided", monkey_collided);
+
     obstacleGroup.setLifetimeEach(-1);
     bananaGroup.setLifetimeEach(-1);
 
@@ -103,11 +131,13 @@ function draw() {
     bananaGroup.setVelocityXEach(0);
 
   }
+  monkey.setCollider("circle", 0, 0, 350);
+
   //to display sprites
   drawSprites();
 
   //to display bananas collected
-  text("Bananas collected:" + score, 450, 30, fill("black"), textSize(15));
+  text("SCORE:" + score, 450, 30, fill("black"), textSize(20));
 }
 
 //making function for banana
@@ -116,7 +146,7 @@ function food() {
     banana = createSprite(600, 300, 20, 20);
     banana.y = Math.round(random(120, 400));
     banana.addImage(bananaImage);
-    banana.velocityX = -3;
+    banana.velocityX = -4;
     banana.lifetime = 300;
     banana.scale = 0.1;
 
